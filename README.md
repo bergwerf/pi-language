a44884a777b812e6a6224386254f1071eaf8cfc1
Herman Bergwerf <hermanbergwerf@gmail.com>
2020-02-15 00:59:44 +0100

    Rewrite RunProc

--- README.md
+++ README.md
@@ -47,7 +47,7 @@ data types. The interface channels are:
 - `stdout_[0-9A-F]{2}` writes bytes to the standard output when triggered.
 - `stdin__[a-zA-Z0-9]` and `stdout__[a-zA-Z0-9]` are aliases.
 - `stdin_EOF` triggers when the stdin EOF is reached.
-- `debug` prints information about any channel sent to it.
+- `DEBUG` prints information about any channel sent to it.
 
 I replaced the replication operator with a subscribe operator which will respawn
 the subsequent process whenever a new element is received on the subscribed
--- run.go
+++ run.go
@@ -16,25 +16,40 @@ type Node struct {
 
 // Msg is a message.
 type Msg struct {
-	Seq    int
-	ID     uint
-	Sender *Proc
+	Seq       uint
+	ChannelID uint
+	MessageID uint
 }
 
-// Channel is a channel message queue.
+// Channel holds channel and subscription information.
 type Channel struct {
-	Seq      int
-	Messages []Msg
+	Seq       uint
+	Listening *list.List
+	PrevCycle uint64
+}
+
+// PrintListeners of a channel.
+func (c *Channel) PrintListeners() {
+	for n := c.Listening.Front(); n != nil; n = n.Next() {
+		node := n.Value.(Node)
+		fmt.Printf("+ %v\n", node.Proc.Location)
+	}
 }
 
 // RunProc runs the given pre-processed program.
 func RunProc(proc []*Proc, input io.Reader, output io.Writer) {
-	// Global channel identifier sequence.
-	channels := reservedChannelIDs
+	queue := make([]Node, 0)
+	ether := make([]Msg, 0)
+	network := make(map[uint]*Channel)
 
-	queue := make([]Node, 0)         // Scheduled nodes
-	ether := make(map[uint]*Channel) // Floating messages
-	sub := make(map[uint]*list.List) // Nodes waiting on a channel
+	channelSeq := uint(0) // Used to number channels.
+	cycle := uint64(0)    // Deliver <=1 message per cycle per channel.
+
+	// Add all interface channels to the network.
+	for channelSeq < reservedChannelIDs {
+		network[channelSeq] = &Channel{0, list.New(), 0}
+		channelSeq++
+	}
 
 	// Queue input program.
 	for _, p := range proc {
@@ -43,15 +58,7 @@ func RunProc(proc []*Proc, input io.Reader, output io.Writer) {
 
 	// Run simulation until no more nodes are scheduled for running time and the
 	// ether is empty (all messages have been delivered).
-	pendingMessages := 0
-	for len(queue)+pendingMessages > 0 {
-		// There are a lot of different ways to decide which process gets to run
-		// next or which message is delivered next. Randomizing this might be
-		// interesting for fuzzing. Trying all permutations could prove the
-		// soundness of a program. And intelligent bookkeeping might allow us to
-		// repress processes that are using a lot of processing time. The README
-		// lists some semantics considerations. This is a fairly naive approach.
-
+	for len(queue)+len(ether) > 0 {
 		// Run all queued nodes.
 		for len(queue) > 0 {
 			var node Node
@@ -59,9 +66,10 @@ func RunProc(proc []*Proc, input io.Reader, output io.Writer) {
 			switch node.Proc.Action {
 			case PINewRef:
 				assert(len(node.Refs) == int(node.Proc.Channel.Value))
-				refs := copyAppend(node.Refs, channels)
+				refs := copyAppend(node.Refs, channelSeq)
 				seqs := copyAppend(node.Seqs, 0)
-				channels++
+				network[channelSeq] = &Channel{0, list.New(), 0}
+				channelSeq++
 				for _, p := range node.Proc.Children {
 					queue = append(queue, Node{p, refs, seqs})
 				}
@@ -69,26 +77,32 @@ func RunProc(proc []*Proc, input io.Reader, output io.Writer) {
 			case PISubsOne:
 				fallthrough
 			case PISubsAll:
-				channel := node.Proc.Channel.ID(node)
-				subscribe(sub, channel, node)
+				channelID := node.Proc.Channel.ID(node)
+				network[channelID].Listening.PushBack(node)
 
 			case PISend:
-				channel := node.Proc.Channel.ID(node)
-				message := node.Proc.Message.ID(node)
+				channelID := node.Proc.Channel.ID(node)
+				messageID := node.Proc.Message.ID(node)
+				channel := network[channelID]
 				seqs := node.Seqs
 
 				// Messages to the debug interface channel are handled immediately. This
 				// is practical because if we wait the subscriber map may change.
-				if channel == specialChannels["DEBUG"] {
-					printDebugInfo(sub, message)
+				if channelID == specialChannels["DEBUG"] {
+					println()
+					println("--- DEBUG SECTION ---")
+					fmt.Printf("channel: %v\n", channelID)
+					channel.PrintListeners()
+					println("---------------------")
 				} else {
 					// Otherwise send and update sequence number for this channel. We do
 					// not set sequence numbers for interface channels.
-					seq := send(ether, channel, message, node.Proc)
+					channel.Seq++
+					ether = append(ether, Msg{channel.Seq, channelID, messageID})
 					if !node.Proc.Channel.Raw {
 						seqs = make([]uint, len(node.Seqs))
 						copy(seqs, node.Seqs)
-						seqs[node.Proc.Channel.Value] = uint(seq)
+						seqs[node.Proc.Channel.Value] = channel.Seq
 					}
 				}
 
@@ -98,115 +112,71 @@ func RunProc(proc []*Proc, input io.Reader, output io.Writer) {
 			}
 		}
 
-		// Deliver some messages.
-		for channel, info := range ether {
-			if len(info.Messages) == 0 {
-				// Do not delete! This will reset the sequence number on this channel
-				// resulting in skipped messages.
+		// Deliver at most one message per channel.
+		cycle++
+		messages := ether
+		ether = ether[0:0]
+		for _, msg := range messages {
+			// Check if we already delivered a message to this channel in this cycle.
+			channel := network[msg.ChannelID]
+			if channel.PrevCycle == cycle {
+				ether = append(ether, msg)
 				continue
 			}
+			channel.PrevCycle = cycle
 
-			// We can clear at most one message per channel each cycle since we have
-			// to let receiving processes refresh their subscription in between.
-			var msg Msg
-			msg, info.Messages = info.Messages[0], info.Messages[1:]
-			if subs, nonEmpty := sub[channel]; nonEmpty {
-				for n := subs.Front(); n != nil; n = n.Next() {
-					node := n.Value.(Node)
-					p := node.Proc
-					assert(p.Channel.ID(node) == channel)
-					assert(len(node.Refs) == int(p.Message.Value))
+			for ptr := channel.Listening.Front(); ptr != nil; ptr = ptr.Next() {
+				node := ptr.Value.(Node)
+				p := node.Proc
+				assert(p.Channel.ID(node) == msg.ChannelID)
+				assert(len(node.Refs) == int(p.Message.Value))
 
-					// Check if this message is after the node specific channel sequence.
-					// We do not do sequence checks on interface channels because you
-					// should not both send and listen on those (perhaps this is a bit of
-					// a silly optimization).
-					if channel >= reservedChannelIDs &&
-						uint(msg.Seq) <= node.Seqs[p.Channel.Value] {
-						continue
-					}
+				// Check if this message is after the node specific channel sequence.
+				if !p.Channel.Raw && msg.Seq <= node.Seqs[p.Channel.Value] {
+					continue
+				}
 
-					// Append message to references and queue child processes.
-					refs := copyAppend(node.Refs, msg.ID)
-					seqs := copyAppend(node.Seqs, 0)
-					for _, p := range p.Children {
-						queue = append(queue, Node{p, refs, seqs})
-					}
+				// Append message to references and queue child processes.
+				refs := copyAppend(node.Refs, msg.MessageID)
+				seqs := copyAppend(node.Seqs, 0)
+				for _, p := range p.Children {
+					queue = append(queue, Node{p, refs, seqs})
+				}
 
-					// Remove PISubsOne subscription.
-					if node.Proc.Action == PISubsOne {
-						subs.Remove(n)
-					}
+				// Remove PISubsOne subscription.
+				if node.Proc.Action == PISubsOne {
+					channel.Listening.Remove(ptr)
 				}
 			}
 
-			// Handle interface messages.
-			handleInterfaceMessage(input, output, ether, channel, msg.ID)
-		}
-
-		// Count number of messages left. Note that handleInterfaceMessage may have
-		// added messages that we could not count before.
-		pendingMessages = 0
-		for _, info := range ether {
-			pendingMessages += len(info.Messages)
+			// Handle interface messages. Note that the way we iterate and overwrite
+			// the ether buffer at the same time is only ok as long as this function
+			// returns at most one message.
+			ether = append(ether, handleInterfaceMessage(input, output, msg)...)
 		}
 	}
 }
 
-func subscribe(sub map[uint]*list.List, channel uint, node Node) {
-	if subs, in := sub[channel]; in {
-		subs.PushBack(node)
-	} else {
-		subs := list.New()
-		subs.PushBack(node)
-		sub[channel] = subs
-	}
-}
-
-func send(ether map[uint]*Channel, channel uint, message uint, s *Proc) int {
-	if c, exists := ether[channel]; exists {
-		c.Seq++
-		c.Messages = append(c.Messages, Msg{c.Seq, message, s})
-		return c.Seq
-	}
-	ether[channel] = &Channel{1, []Msg{Msg{1, message, s}}}
-	return 1
-}
-
-func handleInterfaceMessage(
-	input io.Reader, output io.Writer,
-	ether map[uint]*Channel, channel uint, message uint) {
+func handleInterfaceMessage(input io.Reader, output io.Writer, msg Msg) []Msg {
 	// + Standard input read trigger.
 	// + Standard output byte trigger.
 	// + Debug info channel.
-	if channel == specialChannels["stdin_read"] {
+	id := msg.ChannelID
+	if id == specialChannels["stdin_read"] {
 		// Wait for next byte (or EOF)
 		buf := make([]byte, 1)
 		if _, err := input.Read(buf); err == nil {
 			// Send byte read trigger.
-			send(ether, uint(buf[0]), message, nil)
+			return []Msg{Msg{0, uint(buf[0]), msg.MessageID}}
 		} else if err == io.EOF {
 			// Send EOF trigger.
-			send(ether, specialChannels["stdin_EOF"], message, nil)
+			return []Msg{Msg{0, specialChannels["stdin_EOF"], msg.MessageID}}
 		}
-	} else if stdoutIDOffset <= channel && channel < stdoutIDOffset+256 {
+	} else if stdoutIDOffset <= id && id < stdoutIDOffset+256 {
 		// Write byte to stdout and send write trigger.
-		b := byte(channel - stdoutIDOffset)
+		b := byte(id - stdoutIDOffset)
 		output.Write([]byte{b})
-		send(ether, specialChannels["stdout_write"], message, nil)
+		return []Msg{Msg{0, specialChannels["stdout_write"], msg.MessageID}}
 	}
-}
-
-// Print subscribers of a channel.
-func printDebugInfo(sub map[uint]*list.List, channel uint) {
-	println()
-	println("--- DEBUG SECTION ---")
-	fmt.Printf("channel: %v\n", channel)
-	if subs, nonEmpty := sub[channel]; nonEmpty && subs.Len() > 0 {
-		for n := subs.Front(); n != nil; n = n.Next() {
-			node := n.Value.(Node)
-			fmt.Printf("+ %v\n", node.Proc.Location)
-		}
-	}
-	println("---------------------")
+	return nil
 }
